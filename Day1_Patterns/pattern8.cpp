f079954208c9ea84ee5ee8951c246f4d45f4f8df
AvinashSappati <sappatiavinash@gmail.com>
2025-06-11 11:16:50 +0530

    Pattern9 done

--- Day1_Patterns/pattern8.cpp
+++ Day1_Patterns/pattern8.cpp
@@ -13,19 +13,19 @@ using namespace std;
 
 void patt8(int n)
 {
-      for(int i=5 ; i > 0 ; i--)
+      for(int i= 0 ; i < n ; i++)
       {
-            for (int j = 0 ; j < n-i ; j++)
+            for (int j = 0 ; j < i ; j++)
             {
                   cout << " " ;
             }
 
-            for ( int j = 0 ; j < 2*i - 1 ; j++)
+            for ( int j = 0 ; j < 2*n-(2*i + 1) ; j++)
             {
                   cout << "*";
             }
 
-            for (int j = 0 ; j < n-i ; j++)
+            for (int j = 0 ; j < i ; j++)
             {
                   cout << " " ;
             }
