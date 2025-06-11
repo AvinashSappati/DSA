d100b1054642d0efb733934920762e80aac46b49
AvinashSappati <sappatiavinash@gmail.com>
2025-06-11 14:46:36 +0530

    Checking .exe files

--- Day1_Patterns/pattern11.cpp
+++ Day1_Patterns/pattern11.cpp
@@ -7,3 +7,16 @@
 1 0 1 0 1
 
 */
+#include<bits\stdc++.h>
+using namespace std;
+void patt11(int n )
+{
+      
+}
+int main()
+{
+      int n;
+      cin >> n;
+      patt11(n);
+      return 0;
+}
