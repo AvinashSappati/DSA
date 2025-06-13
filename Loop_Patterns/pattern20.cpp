c3aa792d94fdb113a0d8c834fd68ced670be057f
AvinashSappati <sappatiavinash@gmail.com>
2025-06-13 23:00:16 +0530

    Pattern20 done

--- Loop_Patterns/pattern20.cpp
+++ Loop_Patterns/pattern20.cpp
@@ -16,7 +16,38 @@ using namespace std;
 
 void patt20(int n)
 {
-    
+      for(int i=0;i<n;i++)
+      {
+            for(int j=0;j<=i;j++)
+            {
+                  cout << "*";
+            }
+            for(int j=0;j<2*(n-i-1);j++)
+            {
+                  cout << " ";
+            }
+            for(int j=0;j<=i;j++)
+            {
+                  cout << "*";
+            }
+            cout << endl;
+      }
+      for(int i=1;i<n;i++)
+      {
+            for(int j=0;j<(n-i);j++)
+            {
+                  cout << "*";
+            }
+            for(int j=0;j<2*i;j++)
+            {
+                  cout << " ";
+            }
+            for(int j=0;j<(n-i);j++)
+            {
+                  cout << "*";
+            }
+            cout << endl;
+      }
 }
 int main()
 {
