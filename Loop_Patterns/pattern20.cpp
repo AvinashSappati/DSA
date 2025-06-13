7c2537f758aa1777ef2a4b82b27ac7a91c3ffb05
AvinashSappati <sappatiavinash@gmail.com>
2025-06-13 22:39:06 +0530

    Renaming folder

---
+++ Loop_Patterns/pattern20.cpp
@@ -0,0 +1,27 @@
+/*
+PATTERN-20:
+(n=5)
+*                 * 
+* *             * *
+* * *         * * *
+* * * *     * * * *
+* * * * * * * * * *
+* * * *     * * * *
+* * *         * * *
+* *             * *
+*                 *
+*/
+#include<bits\stdc++.h>
+using namespace std;
+
+void patt20(int n)
+{
+    
+}
+int main()
+{
+      int n;
+      cin >> n;
+      patt20(n);
+      return 0;
+}
