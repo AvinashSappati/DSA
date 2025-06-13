0652b015af13732889c6ab1dcff5223b5f069baa
AvinashSappati <sappatiavinash@gmail.com>
2025-06-13 13:59:05 +0530

    Pattern11 done

--- Day1_Patterns/pattern11.cpp
+++ Day1_Patterns/pattern11.cpp
@@ -11,7 +11,17 @@
 using namespace std;
 void patt11(int n )
 {
-      cout << n;
+      int start = 0;
+      for(int i=0;i<n;i++)
+      {
+            if(i%2 == 0) start=1;
+            for(int j=0;j<=i;j++)
+            {
+                  cout << start;
+                  start = 1 - start;
+            }
+            cout << endl;
+      }
 }
 int main()
 {
