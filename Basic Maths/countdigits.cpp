3e3bb8bddb9f3aa69c2021214b6415ff1a8fbad1
AvinashSappati <sappatiavinash@gmail.com>
2025-06-20 23:43:02 +0530

    Counting Digits

--- Basic Maths/countdigits.cpp
+++ Basic Maths/countdigits.cpp
@@ -2,14 +2,7 @@
 using namespace std;
 int CountDigit(int n)
 {
-      int ld;
-      int cnt = 0;
-      while(n>0)
-      {
-            ld = n%10;
-            n = n/10;
-            cnt++;
-      }
+      int cnt = (int)(log10(n) + 1);
       return cnt;
 }
 int main()
